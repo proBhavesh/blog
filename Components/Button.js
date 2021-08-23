8d27dd9de8bf98b5b2840ce9cfbbc27631af3c13
Bhavesh <bhaveshjat9950@gmail.com>
2021-08-24 01:54:55 +0530

    first commit

---
+++ Components/Button.js
@@ -0,0 +1,18 @@
+const Button = () => {
+	return (
+		<>
+			<h1 className="text-4xl">
+				Quote 1: Solve a problem, then use technology to leverage the
+				solution
+			</h1>
+			<h2 className="text-4xl">
+				2. Use Software as a leverage for the solution in other fields.
+			</h2>
+			<h2 className="text-4xl">
+				3. Be really good at programming
+			</h2>
+		</>
+	);
+};
+
+export default Button;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Button from "../Components/Button.js";
+
+const index = () => {
+  return (
+    <div>
+      <Button />
+      <h2 className="text-7xl text-blue-600">This is From Indexjs</h2>
+    </div>
+  );
+};
+
+export default index;
